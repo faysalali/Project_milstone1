f3fee0b07d8b6e98ce97a09414e0336748253097
Faysalali534 <faysalali534@gmail.com>
2018-07-31 13:31:21 +0500

    email validation added.

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,6 @@
 class Client < ApplicationRecord
   validates :name, :email, presence: true, length: { minimum: 3 }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   
   def country_name
     ISO3166::Country[country]
