b1a03e821e278f10f98c0fc6daac73915cf9845b
Faysalali534 <faysalali534@gmail.com>
2018-07-31 13:37:50 +0500

    only needed routes created for client resource.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :clients
+  resources :clients, only: [:index, :create, :new, :destory, :update, :edit]
   root 'homepage#index'
+  
   namespace :admin do
     root to:'admin#index'
+    
     resources :admin do
       member do
-        get 'change_status'
-        get 'promote_to_manager'
+        get 'update_status'
+        get 'update_role'
       end
     end
+    
   end
 end
