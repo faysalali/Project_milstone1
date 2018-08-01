05ad6032aba25aa95781a93b636205e6f0881ecb
Faysalali534 <faysalali534@gmail.com>
2018-08-01 14:24:15 +0500

    Admin: new action bar added

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,13 +1,9 @@
 module AdminHelper
-  def show_role(user)
-    return if user.disable?
-
-    name, classes = user.user? ? ['Promote to manager', 'btn-primary'] : ['Demote to User', 'btn-danger']
-    link_to name, update_role_admin_user_path(user), class: "btn btn-md #{classes}"
+  def user_status_class(user)
+    user.disable? ? 'danger' : 'primary'
+  end
+  def user_role_class(user)
+    user.manager? ? 'success' : 'primary'
   end
 
-  def show_status(user)
-    name, classes = user.enable? ? ['User Enabled', 'btn-primary'] : ['User Disabled', 'btn-danger']
-    link_to name, update_status_admin_user_path(user), class: "btn btn-md #{classes}"
-  end
 end
