f97ee0a4eb62d56c4ff944879c244a80f8413824
Faysalali534 <faysalali534@gmail.com>
2018-08-08 22:28:44 +0500

    attachments added.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,14 +1,21 @@
 class ProjectsController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :mark_complete, :update]
-  
+  before_action :set_project, only: [:show, :edit, :mark_complete, :update]
+
   def index
     @projects = Project.all
+    @user_projects = current_user.projects
   end
-  
+
+  def show
+    @comments = @project.comments
+    @attachments = @project.attachments
+  end
+
   def new
     @project = Project.new
+    @project.attachments.build
   end
-  
+
   def create
     @project = Project.new(project_params)
     if @project.save
@@ -17,7 +24,7 @@ class ProjectsController < ApplicationController
       render 'new'
     end
   end
-  
+
   def update
     if @project.update(project_params)
       redirect_to projects_path, success: "Client updated successfully!"
@@ -25,20 +32,19 @@ class ProjectsController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def mark_complete
     @project.completed!
     redirect_to projects_path
   end
-  
+
   private
-  
-  def project_params
-    params.require(:project).permit(:title, :description, :price, :end_date, :status, :client_id, user_ids: [])
-  end
-  
-  def set_user
-    @project = Project.find(params[:id])
-  end
-  
+
+    def project_params
+      params.require(:project).permit(:title, :description, :price, :end_date, :status, :client_id, user_ids: [], attachments_attributes: [:id, :file, :_destroy])
+    end
+
+    def set_project
+      @project = Project.find(params[:id])
+    end
 end
